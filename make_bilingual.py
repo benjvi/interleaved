58aeb0c979621eb34e917ce1ee9b8b77088d1eda
Ben Vickers <ben.vickers@opencredo.com>
2018-06-24 20:12:01 +0100

    init commit

---
+++ make_bilingual.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from pydub import AudioSegment
+from pprint import pprint
+from pydub.silence import split_on_silence
+from os import mkdir
+from os.path import exists
+
+fluent_lang = "English"
+learning_lang = "Spanish"
+fluent_lang_code = "EN"
+learning_lang_code = "ES"
+fluent_lang_title = "the-little-prince"
+learning_lang_title = "el-principito"
+fluent_lang_chapter = "chapter"
+learning_lang_chapter = "capitulo"
+
+outdir = "output/{}-{}-{}-test".format(fluent_lang_title, fluent_lang_code, learning_lang_code)
+
+if not exists(outdir):
+    mkdir(outdir)
+
+# because transaltions and emphasis is different between readings and languages, two audio tracks won't make uniform progress through the track
+# we add a fudge factor - a constant percentage by which the foreign language progress should remain ahead of the home language progress
+PROGRESS_FUDGE_FACTOR=0.10
+
+# TODO: can parallelise both by chapter and chunk generation in FR/ES
+for chapter_num in range(1, 2): #28
+    print("Processing chapter {}".format(chapter_num))
+    # SPANISH
+    chapter = AudioSegment.from_mp3("data/{}/{}-{}.mp3".format(learning_lang_title, learning_lang_chapter, chapter_num))
+    es_chunks = split_on_silence(chapter, silence_thresh=-46, keep_silence=100)
+    print("{} chunks: {}".format(learning_lang, len(es_chunks)))
+    es_duration = sum(chunk.duration_seconds for chunk in es_chunks)
+    print("{} chapter length in seconds: {}".format(learning_lang, es_duration))
+
+    # FRENCH
+    chapter = AudioSegment.from_mp3("data/{}/{}-{}.mp3".format(fluent_lang_title, fluent_lang_chapter, chapter_num))
+    fr_chunks = split_on_silence(chapter, min_silence_len=900, silence_thresh=-46, keep_silence=100)
+    print("{} chunks: {}".format(fluent_lang, len(fr_chunks)))
+    fr_duration = sum(chunk.duration_seconds for chunk in fr_chunks)
+    print("{} chapter length in seconds: {}".format(fluent_lang, fr_duration))
+
+    es_progress = 0
+    fr_progress = 0
+    es_play_time = es_chunks[0].duration_seconds
+    fr_play_time = fr_chunks[0].duration_seconds
+    bilingual_audio = AudioSegment.empty()
+    fr_idx = -1
+    es_idx = -1
+    while (fr_idx < len(fr_chunks)-1 or es_idx < len(es_chunks)-1):
+        print(".")
+        # make sure we are making more progress through the book in spanish (always want to encounter spanish text first)
+        es_progress = float(es_play_time)/es_duration
+        fr_progress = float(fr_play_time)/fr_duration
+        print("{} play time is now: {}, progress: {}".format(learning_lang_code, es_play_time, es_progress))
+        print("{} play time is now: {}, progress: {}".format(fluent_lang_code, fr_play_time, fr_progress))
+        if es_idx == -1:
+            es_idx += 1
+            print("Appending {} chunk number {}, of length {}".format(learning_lang_code, es_idx, es_chunks[es_idx].duration_seconds))
+            print("{} play time is now: {}, progress: {}".format(learning_lang_code, es_play_time, es_progress))
+            bilingual_audio += es_chunks[0]
+            es_play_time += es_chunks[es_idx+1].duration_seconds
+        elif (es_progress-PROGRESS_FUDGE_FACTOR > fr_progress or es_idx >= len(es_chunks)-1):
+            fr_idx += 1
+            if fr_idx < len(fr_chunks): 
+                print("Appending {} chunk number {}, of length {}".format(fluent_lang_code, fr_idx, fr_chunks[fr_idx].duration_seconds))
+                print("{} play time is now: {}, progress: {}".format(fluent_lang_code, fr_play_time, fr_progress))
+                bilingual_audio += fr_chunks[fr_idx]
+                if (fr_idx+1 < len(fr_chunks)):
+                    # want calculation of progress in the next iteration to be the projected progress if another FR chunk is chosen
+                    fr_play_time += fr_chunks[fr_idx+1].duration_seconds
+            else:
+                print("Reached last {} chunk in chapter".format(fluent_lang_code))
+        else:
+            es_idx += 1
+            if es_idx < len(es_chunks):
+                print("Appending {} chunk number {}, of length {}".format(learning_lang_code, es_idx, es_chunks[es_idx].duration_seconds))
+                print("{} play time is now: {}, progress: {}".format(learning_lang_code, es_play_time, es_progress))
+                bilingual_audio += es_chunks[es_idx]
+                if (es_idx+1 < len(es_chunks)):
+                    # want calculation of progress in the next iteration to be the projected progress if another ES chunk is chosen
+                    es_play_time += es_chunks[es_idx+1].duration_seconds
+            else:
+               print("Reached last {} chunk in chapter".format(learning_lang_code))
+        bilingual_audio += AudioSegment.silent(duration=3000)
+
+    bilingual_audio.export("{}/chapter-{}.mp3".format(outdir, chapter_num), format="mp3", tags={"album": "The Little Prince Bilingual {}-{}".format(fluent_lang_code, learning_lang_code), "artist": "bv", "title": "principito-ch-{}".format(chapter_num)})
---
+++ split_into_chapters.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from pydub import AudioSegment
+from datetime import timedelta
+
+audiobook_filename = "y2mate.com - audiobooks_the_little_prince_antoine_de_saint_exupery_yWQo_AAHDUA.mp3"
+chapter_segments = [
+  (timedelta(seconds=0), timedelta(minutes=3, seconds=8)),
+  (timedelta(minutes=3, seconds=8), timedelta(minutes=8, seconds=13)),
+  (timedelta(minutes=8, seconds=13), timedelta(minutes=11, seconds=18)),
+  (timedelta(minutes=11, seconds=18), timedelta(minutes=16, seconds=32)),
+  (timedelta(minutes=16, seconds=32), timedelta(minutes=21, seconds=30)),
+  (timedelta(minutes=21, seconds=30), timedelta(minutes=23, seconds=7)),
+  (timedelta(minutes=23, seconds=7), timedelta(minutes=28, seconds=37)),
+  (timedelta(minutes=28, seconds=37), timedelta(minutes=33, seconds=55)),
+  (timedelta(minutes=33, seconds=55), timedelta(minutes=37, seconds=7)), 
+  (timedelta(minutes=37, seconds=7), timedelta(minutes=45, seconds=42)), 
+  (timedelta(minutes=45, seconds=42), timedelta(minutes=47, seconds=51)), 
+  (timedelta(minutes=47, seconds=51), timedelta(minutes=49, seconds=11)), 
+  (timedelta(minutes=49, seconds=11), timedelta(minutes=54, seconds=55)), 
+  (timedelta(minutes=54, seconds=55), timedelta(minutes=59, seconds=51)), 
+  (timedelta(minutes=59, seconds=51), timedelta(hours=1, minutes=5, seconds=47)), 
+  (timedelta(hours=1, minutes=5, seconds=47), timedelta(hours=1, minutes=7, seconds=35)), 
+  (timedelta(hours=1, minutes=7, seconds=35), timedelta(hours=1, minutes=11, seconds=40)), 
+  (timedelta(hours=1, minutes=11, seconds=40), timedelta(hours=1, minutes=12, seconds=34)), 
+  (timedelta(hours=1, minutes=12, seconds=34), timedelta(hours=1, minutes=13, seconds=58)), 
+  (timedelta(hours=1, minutes=13, seconds=58), timedelta(hours=1, minutes=15, seconds=42)), 
+  (timedelta(hours=1, minutes=15, seconds=42), timedelta(hours=1, minutes=25, seconds=22)), 
+  (timedelta(hours=1, minutes=25, seconds=22), timedelta(hours=1, minutes=27, seconds=15)), 
+  (timedelta(hours=1, minutes=27, seconds=15), timedelta(hours=1, minutes=28, seconds=5)), 
+  (timedelta(hours=1, minutes=28, seconds=5), timedelta(hours=1, minutes=33, seconds=18)), 
+  (timedelta(hours=1, minutes=33, seconds=18), timedelta(hours=1, minutes=39, seconds=12)), 
+  (timedelta(hours=1, minutes=39, seconds=12), timedelta(hours=1, minutes=50, seconds=13)), 
+  (timedelta(hours=1, minutes=50, seconds=13), timedelta(hours=1, minutes=57, seconds=37)) 
+]
+
+audiobook = AudioSegment.from_mp3("data/unsanitised/{}".format(audiobook_filename))
+
+for idx, segment in enumerate(chapter_segments, start=1):
+  chapter = audiobook[segment[0].total_seconds()*1000:segment[1].total_seconds()*1000]
+  chapter.export("data/the-little-prince/chapter-{}.mp3".format(idx), format="mp3", tags={"album": "The Little Prince", "artist": "bv", "title": "the-little-prince-ch-{}".format(idx)}) 
